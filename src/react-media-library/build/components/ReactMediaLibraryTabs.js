169b88923cab99261664c07dbf85af08cf8206b7
gordongecco <gordongecco77@gmail.com>
2019-12-07 22:31:50 +0100

    12.7

--- src/react-media-library/build/components/ReactMediaLibraryTabs.js
+++ src/react-media-library/build/components/ReactMediaLibraryTabs.js
@@ -1,15 +1,30 @@
 import * as React from "react";
-import Tabs from "react-bootstrap/Tabs";
-import Tab from "react-bootstrap/Tab";
+import { Tab } from 'semantic-ui-react'
 import FileUpload from "./FileUpload";
 import FileLibrary from "./FileLibrary";
-const ReactMediaLibraryTabs = (props) => {
-    return (React.createElement(Tabs, { defaultActiveKey: "upload", id: "react-media-library-tabs" },
-        React.createElement(Tab, { eventKey: "upload", title: "Upload" },
+
+
+
+const UploadTab = (props)=>{
+return(
             React.createElement("div", { className: "pt-3" },
-                React.createElement(FileUpload, { fileUploadCallback: props.fileUploadCallback }))),
-        (Array.isArray(props.fileLibraryList) && props.fileLibraryList.length > 0) && (React.createElement(Tab, { eventKey: "library", title: "Library" },
-            React.createElement(FileLibrary, { fileLibraryList: props.fileLibraryList, fileSelectCallback: props.fileSelectCallback, fileDeleteCallback: props.fileDeleteCallback, libraryCardComponent: props.libraryCardComponent })))));
+                React.createElement(FileUpload, { fileUploadCallback: props.fileUploadCallback }))
+);};
+
+const LibraryTab = (props)=>{
+    return(
+        (Array.isArray(props.fileLibraryList) && props.fileLibraryList.length > 0) && 
+        (React.createElement(FileLibrary, { fileLibraryList: props.fileLibraryList, fileSelectCallback: props.fileSelectCallback, fileDeleteCallback: props.fileDeleteCallback, libraryCardComponent: props.libraryCardComponent }))
+    );
 };
+
+const panes = [
+    { menuItem: 'Upload', render: UploadTab },
+    { menuItem: 'Library', render:LibraryTab },
+  ];
+  
+const ReactMediaLibraryTabs = (props) => <Tab panes={panes} {...props}/>
+  
+
 export default ReactMediaLibraryTabs;
-//# sourceMappingURL=ReactMediaLibraryTabs.js.map
+// # sourceMappingURL=ReactMediaLibraryTabs.js.map
